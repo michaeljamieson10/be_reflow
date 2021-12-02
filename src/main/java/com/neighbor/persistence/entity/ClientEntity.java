93faf367aaa0aed31bf623fd25e31e4dbd70d19f
michaeljamieson <michaeljamieson10@gmail.com>
2021-12-02 14:42:24 -0500

    added client table and client entity

---
+++ src/main/java/com/neighbor/persistence/entity/ClientEntity.java
@@ -0,0 +1,38 @@
+package com.neighbor.persistence.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy=   GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    @JoinColumn(name = "user_id")
+    @ManyToOne(optional = false)
+    private UserEntity userEntity;
+
+    @Column(name = "is_active")
+    private boolean active = true;
+
+    @Column(name = "created_timestamp")
+    @CreationTimestamp
+    private Timestamp createdTimestamp;
+
+    @Column(name = "updated_timestamp")
+    @UpdateTimestamp
+    private Timestamp updatedTimestamp;
+
+}
